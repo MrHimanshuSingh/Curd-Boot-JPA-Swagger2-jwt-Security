2b7036c30571994d102d70eb43355936330ea5f7
XENA <XENA@LAPTOP-380533IJ>
2023-03-31 23:11:46 +0530

    3rd push

--- CRUD-Boot-JPA-Swagger2-JWT-Security/src/main/java/com/springboot/repository/MyRepo.java
+++ CRUD-Boot-JPA-Swagger2-JWT-Security/src/main/java/com/springboot/repository/MyRepo.java
@@ -1,6 +1,8 @@
-package com.springboot.pojo;
+package com.springboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.pojo.Customer;
 
 public interface MyRepo extends JpaRepository<Customer, String> {
 
