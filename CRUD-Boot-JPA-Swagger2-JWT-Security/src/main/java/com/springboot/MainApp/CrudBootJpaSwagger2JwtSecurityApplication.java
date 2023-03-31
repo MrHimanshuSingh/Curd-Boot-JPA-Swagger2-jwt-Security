23021b7a53279adcf5b0d810e59cd52fc7a26693
XENA <XENA@LAPTOP-380533IJ>
2023-03-31 22:13:22 +0530

    1st commit

---
+++ CRUD-Boot-JPA-Swagger2-JWT-Security/src/main/java/com/springboot/MainApp/CrudBootJpaSwagger2JwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.MainApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBootJpaSwagger2JwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBootJpaSwagger2JwtSecurityApplication.class, args);
+	}
+
+}
