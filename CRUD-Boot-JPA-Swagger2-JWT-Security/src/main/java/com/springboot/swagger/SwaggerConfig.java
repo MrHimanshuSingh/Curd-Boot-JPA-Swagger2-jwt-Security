1800a58d3a6f64b3861dcd1c090c5bf022c94215
XENA <XENA@LAPTOP-380533IJ>
2023-04-22 09:45:26 +0530

    swagger Successfully implemented

--- CRUD-Boot-JPA-Swagger2-JWT-Security/src/main/java/com/springboot/swagger/SwaggerConfig.java
+++ CRUD-Boot-JPA-Swagger2-JWT-Security/src/main/java/com/springboot/swagger/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.springboot.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,9 +14,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //@EnableWebMvc
 public class SwaggerConfig {
 
-//	@Bean
-//	protected Docket api() {
-//		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-//				.paths(PathSelectors.any()).build();
-//	}
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.springboot"))
+				.paths(PathSelectors.any()).build();
+	}
 }
