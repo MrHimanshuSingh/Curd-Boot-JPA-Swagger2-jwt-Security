a121c6f8d68b84f231874659a119f92718086744
XENA <XENA@LAPTOP-380533IJ>
2023-04-28 22:49:32 +0530

    logger

--- CRUD-Boot-JPA-Swagger2-JWT-Security/src/main/java/com/springboot/CrudBootJpaSwagger2JwtSecurityApplication.java
+++ CRUD-Boot-JPA-Swagger2-JWT-Security/src/main/java/com/springboot/CrudBootJpaSwagger2JwtSecurityApplication.java
@@ -14,10 +14,10 @@ public class CrudBootJpaSwagger2JwtSecurityApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(CrudBootJpaSwagger2JwtSecurityApplication.class, args);
 		MyService service = (MyService) context.getBean(MyServiceImpl.class);
-//		for (int a = 1; a <= 5; a++) {
-//			Customer customer = new Customer( a,"Name" + a, "Surname" + a, (a % 2 == 0) ? "Male" : "Female","hima"+a+"@gmail.com");
-//			service.add(customer);
-//		}
+		for (int a = 1; a <= 5; a++) {
+			Customer customer = new Customer( a,"Name" + a, "Surname" + a, (a % 2 == 0) ? "Male" : "Female","hima"+a+"@gmail.com");
+			service.add(customer);
+		}
 	}
 
 }
--- CRUD-Boot-JPA-Swagger2-JWT-Security/src/main/java/com/springboot/service/MyServiceImpl.java
+++ CRUD-Boot-JPA-Swagger2-JWT-Security/src/main/java/com/springboot/service/MyServiceImpl.java
@@ -1,56 +1,106 @@
 package com.springboot.service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springboot.pojo.Customer;
 import com.springboot.repository.MyRepo;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class MyServiceImpl implements MyService {
 
 	@Autowired
 	private MyRepo jpaRepository;
 
+	@Transactional
 	@Override
 	public String add(Customer customer) {
-		Optional<Customer> optCust = get(customer.getCustomer_Email());
-		if (optCust.isEmpty())
-			return (jpaRepository.save(customer) != null) ? "Inserted" : "NotInserted";
-		else
-			return "AlreadyExisted";
+		try {
+
+			log.debug("MyServiceImpl >> get() >> processing");
+			Optional<Customer> optCust = get(customer.getCustomer_Email());
+			if (optCust.isEmpty()) {
+				String status = (jpaRepository.save(customer) != null) ? "Inserted" : "NotInserted";
+				log.info("MyServiceImpl >> update() >> {} ", customer.getCustomer_Name() + " is " + status);
+				return status;
+			} else {
+				log.info("MyServiceImpl >> update() >> {} AlreadyExist", customer.getCustomer_Name());
+				return "AlreadyExisted";
+			}
+		} catch (Exception e) {
+			log.error(e.toString());
+			return "Something went wrong";
+		}
 
 	}
 
+	@Transactional
 	@Override
 	public String update(Customer customer) {
-		Optional<Customer> optCust = get(customer.getCustomer_Email());
-		if (optCust.isPresent())
-			return (jpaRepository.save(customer) != null) ? "Updated" : "NotUpdated";
-		else
-			return "NotExist";
+		try {
+			log.debug("MyServiceImpl >> update() >> processing");
+			Optional<Customer> optCust = get(customer.getCustomer_Email());
+			if (optCust.isPresent()) {
+				log.info("MyServiceImpl >> update() >> {} update", customer.getCustomer_Name());
+				return (jpaRepository.save(customer) != null) ? "Updated" : "NotUpdated";
+			} else {
+				log.info("MyServiceImpl >> update() >> {} NotExist", customer.getCustomer_Name());
+				return "NotExist";
+			}
+		} catch (Exception e) {
+			log.error(e.toString());
+			return "Something went wrong";
+
+		}
 	}
 
 	@Override
 	public Optional<Customer> get(String email) {
-		return jpaRepository.findById(email);
+		try {
+			log.info("MyServiceImpl >> get() >> processing");
+			return jpaRepository.findById(email);
+		} catch (Exception e) {
+			log.error(e.toString());
+			return Optional.empty();
+		}
+	}
+
+	@Transactional
+	@Override
+	public String delete(String email) {
+		try {
+			log.debug("MyServiceImpl >> delete() >> processing");
+			Optional<Customer> optCust = get(email);
+			if (optCust.isPresent()) {
+				jpaRepository.deleteById(email);
+				log.info("MyServiceImpl >> delete() >> {} Deleted", email);
+				return "deleted";
+			} else {
+				log.info("MyServiceImpl >> delete() >> {} NotExist", email);
+				return "NotExist";
+			}
+		} catch (Exception e) {
+			log.error(e.toString());
+			return "Something went wrong";
+		}
 	}
 
 	@Override
-	public String delete(String email) {
-		Optional<Customer> optCust = get(email);
-		if (optCust.isPresent()) {
-			jpaRepository.deleteById(email);
-			return "deleted";
-		} else
-			return "NotExist";
-	}
-	
-	@Override
 	public List<Customer> getAll() {
-		return jpaRepository.findAll();
+		try {
+			log.info("MyServiceImpl >> getAll() >> processing");
+			return jpaRepository.findAll();
+		} catch (Exception e) {
+			log.error(e.toString());
+			return Collections.emptyList();
+		}
 	}
 }
